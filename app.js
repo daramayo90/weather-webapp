2678e7eda0ee3facfb18d54de02fef4351a09864
daramayo90 <da.aramayo1990@gmail.com>
2022-03-25 17:07:03 -0500

    weather-webapp: folder structure

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './src/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/views/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
