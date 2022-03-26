da2cf630f25b943006475ce8b2c87f173e05d6aa
daramayo90 <da.aramayo1990@gmail.com>
2022-03-25 21:30:56 -0500

    Testing deployment

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var pages = require("node-github-pages")(app, {
+    static: "public", // Static directory path(css, js...)
+    path: "docs" // Output path
+  });
+  pages.renderFiles([{
+    "view": "index",
+    "url": "",
+    "options": { title: "Express" }
+  },
+  {
+    "view": "second",
+    "url": "/second",
+    "options": { title: "second page" }
+  },
+  ]);
+
 app.use(express.static(path.join(__dirname, './src/public')));
 
 app.get('/', (req, res) => {
